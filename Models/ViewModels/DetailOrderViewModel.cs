4fe2ef1d62c3049f9fadd5dead84d546e1156d41
BichHaoHi <21522049@gm.uit.edu.vn>
2023-12-30 00:11:18 +0700

    upload layout

---
+++ Models/ViewModels/DetailOrderViewModel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class DetailOrderViewModel
+    {
+        public List<OrderDetail> orderDetails {set; get;}
+        public Order order {set; get;}
+        public User? user {set; get;}
+        public List<string> listStatus = new List<string>{
+            "Đã đặt hàng",
+            "Đang chuẩn bị",
+            "Đang giao hàng",
+            "Đợi xác nhận hủy",
+            "Đã hủy",
+            "Thành công"
+        };
+    }
+}
--- ProjectFrame/Program.cs
+++ ProjectFrame/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContextConnection"))
     );
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>().AddSignInManager<SignInManager<IdentityUser>>()
